bd78a57ae76c5c32fa04346ec09a33fa41f0aee1
Rio Bérénos <r.berenos@hotmail.com>
2024-07-29 16:09:22 +0200

    added a todo

--- background.js
+++ background.js
@@ -8,7 +8,7 @@ browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
         return;
     }
 
-    const urlPattern = new RegExp("^https?://.*skin\\.club/.*/cases/open/.*");
+    const urlPattern = new RegExp("^https?://skin\\.club/.*/cases/open/.*");
     if (urlPattern.test(tab.url)) {
         browser.tabs.executeScript(tabId, {file: "content_script.js"});
     }
