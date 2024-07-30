836c2c9ce3ff03860e7c881d0d248ff7c7b074ff
Rio Bérénos <r.berenos@hotmail.com>
2024-07-30 02:07:36 +0200

    added case battle calculator

--- cscripts/cscript_case_battle.js
+++ cscripts/cscript_case_battle.js
@@ -3,10 +3,36 @@ if (typeof SkinClubCaseBattleCalculator === "undefined") {
 
     class SkinClubCaseBattleCalculator {
         constructor() {
+            this.moneyCounterClass = "CLASS__MC-EL-SCPC";
+
+            this.battleProgressQuery = ".battle-progress";
+            this.battleSlotQuery = ".battle-slot";
+            this.battleInventoryQuery = "aside.battle-slot__aside .battle-slot-inventory";
+            this.battleDropQuery = ".battle-inventory-drop";
+
+            this.itemPriceQuery = ".drop-main-info__price";
+            this.itemEventPointsQuery = ".battle-slot-inventory-event-points__text";
+
+            this.playerInfoContainerQuery = ".battle-slot-player__player-info.player-info";
+            this.playerInfoQuery = ".player-info__main";
+            this.playerUsernameContainerQuery = ".player-info__username";
+
             this.init();
         }
 
         init() {
+            this.onElementAppear(this.battleProgressQuery).then(() => {
+                const observer = new MutationObserver((mutations, observer) => this._observerCallback(mutations, observer));
+                const options = {
+                    childList: true,
+                    subtree: false
+                };
+
+                const caseInventories = document.querySelectorAll(this.battleInventoryQuery);
+                caseInventories.forEach(caseInventory => observer.observe(caseInventory, options));
+
+                this.updateMoneyCounters();
+            });
         }
 
         onElementAppear(query) {
@@ -33,6 +59,69 @@ if (typeof SkinClubCaseBattleCalculator === "undefined") {
             });
         }
 
+        updateMoneyCounters() {
+            // calculate player scores
+            let playerScores = [];
+
+            const players = document.querySelectorAll(this.battleSlotQuery);
+            players.forEach(player => {
+                let worth = 0.0;
+
+                const inventory = player.querySelectorAll(this.battleDropQuery);
+                inventory.forEach(item => {
+                    const isEventPointsItem = item.querySelector(this.itemEventPointsQuery) !== null;
+                    if (isEventPointsItem) {
+                        return;
+                    }
+
+                    const price = parseFloat(item.querySelector(this.itemPriceQuery).innerText.replace("$", ""));
+                    worth += price;
+                });
+
+                const name = player.querySelector(this.playerInfoQuery).textContent;
+                playerScores.push({
+                    name: name,
+                    value: worth
+                });
+            });
+
+            playerScores.sort((a, b) => b.value - a.value);
+            const leader = playerScores[0];
+
+            // user feedback
+            players.forEach(player => {
+                const playerInfoContainer = player.querySelector(this.playerInfoContainerQuery)
+                const playerInfo = playerInfoContainer.querySelector(this.playerInfoQuery)
+                const name = playerInfo.textContent;
+                const inventoryValue = playerScores.find(player => player.name === name).value;
+
+                // highlight player
+                if (name === leader.name) {
+                    playerInfo.querySelector(this.playerUsernameContainerQuery).style.color = "yellow";
+                } else {
+                    playerInfo.querySelector(this.playerUsernameContainerQuery).style = "";
+                }
+
+                // money counter
+                let moneyCounter = playerInfoContainer.querySelector(`.${this.moneyCounterClass}`);
+                if (!moneyCounter) {
+                    moneyCounter = document.createElement("div");
+                    moneyCounter.classList.add(this.moneyCounterClass);
+                    playerInfoContainer.appendChild(moneyCounter);
+                }
+                moneyCounter.innerHTML = `$${inventoryValue.toFixed(2)}`;
+            });
+        }
+
+        _observerCallback(mutations, observer = null) {
+            for (const mutation of mutations) {
+                if (mutation.addedNodes && mutation.addedNodes.length > 0) {
+                    // make sure at least some items were added before updating
+                    this.updateMoneyCounters();
+                    break;
+                }
+            }
+        }
     }
 
     new SkinClubCaseBattleCalculator();
